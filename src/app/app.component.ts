d351db68945303859076dd5e2737b5f8cffde51b
kwiljac <109763824+kwiljac@users.noreply.github.com>
2022-08-14 10:10:07 -0400

    First build

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'assignment-1';
+
+  storedPosts: any[] = [];
+
+  onPostAdded(post: any) {
+    this.storedPosts.push(post);
+  }
 }
